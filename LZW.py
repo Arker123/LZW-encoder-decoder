9f89896733c05920a382a30e05ceecb8c04f20d7
Arker123 <kharbandarnav@gmail.com>
2023-04-20 01:44:32 +0530

    Done :)

--- LZW.py
+++ LZW.py
@@ -4,29 +4,223 @@ from PIL import Image
 from matplotlib import pyplot as plt
 import itertools
 import math
+import skimage.measure    
+from scipy.stats import entropy
 
 class LZW:
-    def __init__(self, image):
-        self.image = image
-        self.height = image.shape[0]
-        self.width = image.shape[1]
+    def __init__(self):
+
+        # self.height = image.shape[0]
+        # self.width = image.shape[1]
         self.dictionary = {}
         self.dictionary_size = 256
         self.compressed = []
         self.decompressed = []
         self.entropy = 0
         self.comp = 0
+        self.block_size = 8
 
-    def compress(self, uncompressed):
+    def compress_image(self, image, block_size, codes_size):
+        # print(self.isGrayscale(image))
+
+        self.image = image
+
+        
+
+        if not(self.isGrayscale(image)):
+            image = np.asarray(image)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        else:
+            image = np.asarray(image)
+
+        nparray = np.asarray(image)
+        
+
+        new_width = math.ceil(self.image.size[0]/self.block_size)*self.block_size
+        new_height = math.ceil(self.image.size[1]/self.block_size)*self.block_size
+
+        new_height = new_height - self.image.size[1]
+        new_width = new_width - self.image.size[0]
+        
+        image = np.pad(nparray, ((0,new_height),(0,new_width)), mode='constant') 
+        
+
+        self.shape = image.shape
+
+        if block_size != -1:
+            self.block_size = block_size
+            blocks = self.split_into_blocks(image, block_size)
+        else:
+            self.block_size = block_size
+            blocks = [self.nparray_to_list(image)]
+            # blocks = [image]
+
+        # print(blocks)
+
+        # print(blocks[160])
+
+        compressed_blocks = []
+        for block in blocks:
+            # print("hey")
+            compressed_block = self.compress_block(block)
+            compressed_blocks.append(compressed_block)
+        
+        # print(compressed_blocks)
+        self.encoded_image = compressed_blocks
+        self.encoded_copy = compressed_blocks
+
+        max = 0
+        for block in compressed_blocks:
+            for element in block:
+                if element > max:
+                    max = element
+
+        if max > 2**codes_size:
+            assert False, "Codes size is too small"
+            return
+
+        self.write_encoded_data('compressed.txt')
+
+        return compressed_blocks, max
+        # print(blocks)
+
+    def decompress_image(self):
+
+        blocks = self.encoded_image
+
+        # print(blocks[160])
+        
+        decompressed_blocks = []
+
+
+        for block in blocks:
+            # print(block)
+            decompressed_block = self.decompress(block)
+            refractor = []
+            for element in decompressed_block:
+                temp = element.split('-')
+                for i in temp:
+                    refractor.append(i)
+            decompressed_blocks.append(refractor)
+            # decompressed_blocks.append(decompressed_block)
+            # break
+
+        # print(decompressed_blocks[160])
+
+        # print(blocks[0])
+
+        # refractor = []
+        # for block in decompressed_blocks:
+        #     for element in block:
+        #         temp = element.split('-')
+        #         for i in temp:
+        #             refractor.append(i)
+
+        # print(refractor)
+        # decompressed_blocks = refractor
+
+        new_img = np.zeros((self.shape[0], self.shape[1]))
+
+        # print(len(decompressed_blocks))
+        # print(self.block_size)
+
+        ct = 0
+
+        if self.block_size != -1:
+
+            for i in range(0, self.shape[0], self.block_size):  
+                for j in range(0, self.shape[1], self.block_size):
+                        # print(i, j) # 40, 0
+                        temp_img = np.zeros((self.block_size, self.block_size))
+                        for k in range(0, self.block_size):
+                            for l in range(0, self.block_size):
+                                # print(k, l, temp_img.shape, ct, self.block_size)
+                                temp_img[k][l] = decompressed_blocks[ct][k*self.block_size + l]
+                        new_img[i:i+self.block_size, j:j+self.block_size] = temp_img
+                        # new_img[i:i+self.block_size, j:j+self.block_size] = decompressed_blocks[ct]
+                        ct+=1
+            #             tiles.append(tile)
+            #             c += 1
+                    
+        else:
+            # print(decompressed_blocks)
+            new_img = decompressed_blocks[0]
+            new_img = np.reshape(new_img, (self.shape[0], self.shape[1]))
+            # print(new_img.shape)
+        
+        # print(new_img)
+        # print(np.int32(self.YUV2RGB(new_img)))
+        # self.decoded_image = np.int32(self.YUV2RGB(new_img))
+        self.decoded_image = np.int32(new_img)
+        self.copy_decoded = self.decoded_image.copy()
+        return self.decoded_image
+        
+        return decompressed_blocks
+    
+    def decompress_image_from_file(self, file):
+        """
+        This function will read the compressed file and decompress it
+        """
+        with open(file, 'r') as f:
+            lines = f.readlines()
+            # print(lines[0])
+            info = lines[0].split(' ')
+            block_size = int(info[2])
+            height = int(info[0])
+            width = int(info[1])
+            # print(block_size, height, width)
+
+            self.shape = (height, width)
+            self.block_size = block_size
+
+            # print(lines[1].strip().split(','))
+
+            # for k in lines[1].strip().split(','):
+                
+                # print(k.replace("[", "").replace(" ", "").replace("]", ""))
+
+            compressed_blocks = []
+            for i in range(1, len(lines)):
+                block = []
+                for elem in lines[i].strip().split(','):
+                    temp = elem.replace("[", "").replace(" ", "").replace("]", "")
+                    block.append(int(temp))
+                compressed_blocks.append(block)
+            
+            self.encoded_image = compressed_blocks
+
+            # print(compressed_blocks)
+
+            return self.decompress_image()
+    
+    def nparray_to_list(self, array):
+        final_array = []
+        for i in range(array.shape[0]):
+            for j in range(array.shape[1]):
+                final_array.append(str(array[i][j]))
+
+        return final_array
+
+    def isGrayscale(self, image):
+        if(image.mode == 'L'):
+            return True
+        elif image.mode=='RGB':
+            return False
+
+    def compress_block(self, uncompressed):
         
 
         dict_size = 256
-        dictionary = dict((str(i), i) for i in range(dict_size))
+        dictionary = dict((str(i), str(i)) for i in range(dict_size))
 
         w = ""
         result = []
         for c in uncompressed:
-            wc = w + c
+            # wc = w + c
+            if w == "":
+                wc = c
+            else:
+                wc = c + '-' + w
             if wc in dictionary:
                 w = wc
             else:
@@ -37,49 +231,89 @@ class LZW:
 
         if w:
             result.append(dictionary[w])
-        return result
+
+        final = []
+        for element in result:
+            final.append(int(element))
+        return final
+    
+    def split_into_blocks(self, image, block_size):
+
+        tiles = []
+
+        for i in range(0, image.shape[0], block_size):  
+            for j in range(0, image.shape[1], block_size):
+
+                    tile = image[i:i+block_size, j:j+block_size]
+                    tiles.append(self.nparray_to_list(tile))
+
+        return tiles
     
     def decompress(self, compressed):
         """Decompress a list of output ks to a string."""
-        from io import StringIO
 
         # Build the dictionary.
         dict_size = 256
         dictionary = dict((i, str(i)) for i in range(dict_size))
-        # in Python 3: dictionary = {i: chr(i) for i in range(dict_size)}
-        
-    #     print(dictionary)
 
-        # use StringIO, otherwise this becomes O(N^2)
-        # due to string concatenation in a loop
         res = []
-        result = StringIO()
         w = str(compressed.pop(0))
-        result.write(w)
         res.append(w)
-    #     print(result.getvalue())
         for k in compressed:
-    #         print(k)
             if k in dictionary:
-    #             print("HEHE")
                 entry = dictionary[k]
             elif k == dict_size:
-    #             print("HERE")
-    #             print(w)
                 entry = w + '-' + w.split('-')[0]
             else:
                 raise ValueError('Bad compressed k: %s' % k)
-            result.write(entry)
-            res.append(entry)
-
-            # Add w+entry[0] to the dictionary.
+            res.append(entry)            
             
-            
-    #         print("Entry: ", entry)
             dictionary[dict_size] = w + '-' + entry.split('-')[0]
             dict_size += 1
 
             w = entry
-    #     print(dictionary)
         return res
     
+    def write_encoded_data(self, file):
+        """Write encoded data to text file"""
+        with open(file, 'w') as f:
+            f.write(str(self.shape[0]) + ' ' + str(self.shape[1]) + ' ' + str(self.block_size) + '\n')
+            for i in range(len(self.encoded_image)):
+                f.write(str(self.encoded_image[i]) + '\n')
+
+    def getCompressionRatio(self):
+        input = self.shape[0] * self.shape[1] * 8
+        l = 0
+        for i in range(len(self.encoded_copy)):
+            l += len(self.encoded_copy[i])
+        output = l*8
+        # print(input, output)
+        return input/output
+    
+    def getEntropy(self):
+        N = 256
+        hist, _ = np.histogram(self.image, N, range=(0, N-1))
+        p_dist = hist /np.sum(hist)
+        en = entropy(p_dist, base=2)
+        
+        return en
+    
+        # N = 256
+        # hist, _ = np.histogram(self.image.ravel(), N, range=(0,N-1))
+        # p_dist = hist/np.sum(hist)
+        # en = entropy(p_dist,)
+        # # print(en)
+        # return en
+    def get_avg_length_encoded_pixels(self):
+        l = 0
+        for i in range(len(self.encoded_copy)):
+            l += len(self.encoded_copy[i])
+        return l/len(self.encoded_copy)
+    
+    def get_total_no_of_codes(self):
+        l = 0
+        # print(self.encoded_copy[0])
+        for i in range(len(self.encoded_copy)):
+
+            l += len(self.encoded_copy[i])
+        return l
--- main.py
+++ main.py
@@ -4,8 +4,77 @@ from PIL import Image
 from matplotlib import pyplot as plt
 import itertools
 import math
+from LZW import LZW
+import os
 
 if __name__ == "__main__":
-    img = Image.open('./test_images/lena.tif')
-    bw_image = np.asarray(img)
-    print(bw_image.shape)
+
+    img = Image.open('./test_images/Fig81b.tif')
+    lzw = LZW()
+    compressed, max = lzw.compress_image(img, 8, 16)
+    lzw.write_encoded_data('compressed.txt')
+    print(compressed)
+    decompressed = lzw.decompress_image_from_file('compressed.txt')
+    plt.imshow(decompressed, cmap='gray')
+    plt.show()
+        
+
+    # #     lzw.write_encoded_data('compressed.txt')
+    # #     # print(compressed)
+    # #     decompressed = lzw.decompress_image_from_file('compressed.txt')
+    # #     # decompressed = lzw.decompress_image()
+    # #     # print(decompressed[0])
+    # #     plt.imshow(decompressed, cmap='gray')
+    # #     # plt.show()
+    # #     print()
+    # #     print(filename)
+    # #     print("Max value: ", max)
+    # #     print("Number of codes used: ", lzw.get_total_no_of_codes())
+    # #     print("Compression Ratio: ", lzw.getCompressionRatio())
+    # #     print("Average length of encoded pixels: ", lzw.get_avg_length_encoded_pixels())
+    # #     print("Entropy: ", lzw.getEntropy())
+    # #     print()
+
+    # L = [4, 8, 16, 32, 64, 128]
+    # CR = []
+
+    # for i in L:
+    #     img = Image.open('./test/kodim01.png')
+    #     # img = Image.open('./test_images/Fig81b.tif')
+    #     lzw = LZW()
+    #     compressed, max = lzw.compress_image(img, i, 32)
+        
+
+    #     # lzw.write_encoded_data('compressed.txt')
+    #     # print(compressed)
+    #     decompressed = lzw.decompress_image_from_file('compressed.txt')
+    #     CR.append(lzw.getCompressionRatio())
+    #     # decompressed = lzw.decompress_image()
+    #     # print(decompressed[0])
+    #     # plt.imshow(decompressed, cmap='gray')
+    #     # plt.show()
+    #     # print()
+    #     # print("Block size: ", i)
+    #     # print("Max value: ", max)
+    #     # print("Number of codes used: ", lzw.get_total_no_of_codes())
+    #     # print("Compression Ratio: ", lzw.getCompressionRatio())
+    #     # print("Average length of encoded pixels: ", lzw.get_avg_length_encoded_pixels())
+    #     # print("Entropy: ", lzw.getEntropy())
+    #     # print()
+        
+    # plt.plot(L, CR, label='Compression Ratio')
+    # plt.xlabel('Block size')
+    # plt.ylabel('Compression Ratio')
+    # plt.title('Block size vs Compression Ratio')
+    # plt.legend()
+    # plt.show()
+    # # l = 0
+    # # for i in lzw.compress_image(img, 8, 12):
+    # #     for j in i:
+    # #         l += 1
+    # # print(l)
+    # # print(len(lzw.compress_image(img, 8, 12)))
+
+    
+    # # bw_image = np.asarray(img)
+    # # print(bw_image.shape)
